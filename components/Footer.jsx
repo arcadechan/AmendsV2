aff7e0043d7788af2469f610ecd38f8456cd59fe
arcade <arcadechan@gmail.com>
2022-01-05 23:38:02 -0800

    Made fixes for SSG. - Updated Header logo to use Next/Link - Updated Head crossOrigin - Updated Footer links with norefferer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,22 +11,22 @@ const Footer = () => {
                     <img src='/badges/large/bookmark_this_page.gif' alt='' />
                     <img src='/badges/large/css.gif' alt='' />
                     <img src='/badges/large/cuteanimegirls.gif' alt='' />
-                    <a href='https://www.w3schools.com/' target='_blank'>
+                    <a href='https://www.w3schools.com/' target='_blank' rel="noreferrer">
                         <img src='/badges/large/html.gif' alt='' />
                     </a>
                     <img src='/badges/large/i_use_ms_paint.gif' alt='' />
                     <img src='/badges/large/made_with_windows.gif' alt='' />
                     <img src='/badges/large/neocities.gif' alt='' />
-                    <a href='https://www.youtube.com/watch?v=9Gj47G2e1Jc' target='_blank'>
+                    <a href='https://www.youtube.com/watch?v=9Gj47G2e1Jc' target='_blank' rel='noreferrer'>
                         <img src='/badges/large/plastic_love_now.png' alt='' />
                     </a>
-                    <a href='https://code.visualstudio.com/' target='_blank'>
+                    <a href='https://code.visualstudio.com/' target='_blank' rel="noreferrer">
                         <img src='/badges/large/vs_code.gif' alt='' />
                     </a>
                     <img src='/badges/large/www.gif' alt='' />
                 </div>
                 <div id='small-badges'>
-                    <a href='https://github.com/arcadechan/AmendsV2' target='_blank'>
+                    <a href='https://github.com/arcadechan/AmendsV2' target='_blank' rel='noreferrer'>
                         <img src='/badges/small/on_github.png' alt='' />
                     </a>
                 </div>
