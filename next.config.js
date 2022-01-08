23b1433838e3bd8373a1cf6448a55e3822f15b75
arcade <arcadechan@gmail.com>
2022-01-08 00:46:29 -0800

    Added markdown support.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] }
+    })
+    return cfg;
+  }
 }
