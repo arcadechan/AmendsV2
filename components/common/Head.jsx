8808eb98174a0f892d0e0b0fc149d6935a9f7341
arcade <arcadechan@gmail.com>
2022-01-11 19:29:32 -0800

    Created custom _document file to hold font, icon, and preconnect settings.

--- components/common/Head.jsx
+++ components/common/Head.jsx
@@ -3,12 +3,8 @@ import { default as NextHead } from "next/head";
 const Head = ({ title, description }) => {
     return (
         <NextHead>
-            <title>{ title || 'Amends' }</title>
-            <meta name="description" content={ description || "Modifying the world's cultural palate one post at a time." } />
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
-            <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;1,400;1,500&family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet"/> 
-            <link rel="icon" href="/favicon.png" />
+            <title>{title || 'Amends'}</title>
+            <meta name="description" content={description || "Modifying the world's cultural palate one post at a time."} />
         </NextHead>
     )
 }
