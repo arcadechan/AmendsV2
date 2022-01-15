147f8b9d983be78602a83f68ea5c6686dd17b08b
arcade <arcadechan@gmail.com>
2022-01-15 03:11:27 -0800

    Added missing exportPathMap - Including dynamic route fetching. - Temporarily enabled trailingSlash due to routing error.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,33 @@
 const path = require('path')
+const fs = require('fs/promises')
 
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')]
+  },
+  exportPathMap: async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ){
+    const paths = {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/posts': { page: '/posts' }
+    }
+
+    const posts = await fs.readdir(path.join('content/posts'))
+    
+    posts.forEach( filename => {
+      const slug = filename.replace('.md', '');
+
+      paths[`/posts/${slug}`] = {
+        page: '/posts/[slug]'
+      }
+    });
+
+    return paths;
   },
   webpack: (cfg) => {
     cfg.module.rules.push({
